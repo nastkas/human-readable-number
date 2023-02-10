638e1c10c17b380123369ccde93e967115640f20
nastkas <nastya.by@mail.ru>
2023-02-10 23:34:26 +0100

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+
+    let words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let thousands = ['thousand', 'million', 'billion', 'trillion'];
+    
+    if (num < 20) return words[num];
+    if (num < 100) return tens[Math.floor(num / 10) - 2] + (num % 10 ? ' ' + toReadable(num % 10) : '');
+    if (num < 1000) return words[Math.floor(num / 100)] + ' hundred' + (num % 100 ? (' '+toReadable(num % 100)) : '');
+    
+    let k = 0;
+    while (num >= 1000) {
+        num /= 1000;
+        k++;
+    }
+    
+    return toReadable(Math.floor(num)) + ' ' + thousands[k - 1] + (num % 1 ? ' ' + toReadable(Math.floor(num * 1000) % 1000) : '');
+        
 }
